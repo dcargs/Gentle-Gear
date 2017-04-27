4ca2fffa9a1a5ead28fb5742d900091121d56c50
dcargs <cargilldevin@gmail.com>
2017-04-27 13:38:24 -0500

    maybe

--- shop/shop.php
+++ shop/shop.php
@@ -17,21 +17,15 @@
   //               </div>
   //             </div>";
 
-
-     foreach ($result as $data) {
-       $content .= $data;
-     }
-
-
-  // $pictures = "<div id='items'>";
-  // if($result->num_rows > 0){
-  //   $pictures += "<div class='row'>";
-  //   while($row = $result->fetch_array(MYSQLI_NUM)){
-  //     $pictures += "<div class='col-md-3 block'><img class='img-responsive' src='../$row[5]' alt='$row[6]'></div>";
-  //   }
-  // }
-  // $pictures += "</div></div>";
-  // $content = $pictures;
+  $pictures = "<div id='items'>";
+  if($result->num_rows > 0){
+    $pictures .= "<div class='row'>";
+    while($row = $result->fetch_array(MYSQLI_NUM)){
+      $pictures .= "<div class='col-md-3 block'><img class='img-responsive' src='../$row[5]' alt='$row[6]'></div>";
+    }
+  }
+  $pictures .= "</div></div>";
+  $content = $pictures;
   $script = '<script src="../statics/scripts.js"></script>';
   $header = "Shop";
   require "statics/layout.php";
