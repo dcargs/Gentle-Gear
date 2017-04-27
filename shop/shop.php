d70401a2249ab41ced779da8174815cbb3661050
dcargs <cargilldevin@gmail.com>
2017-04-27 13:14:37 -0500

    asdf

--- shop/shop.php
+++ shop/shop.php
@@ -2,30 +2,30 @@
   include "shopController.php";
 
   $itemsArray = getItems();
-  $pictures = '<div class="row">
-                <div class="col-md-3 block">
-                  <img class="img-responsive" src="../statics/images/shop/bikeHappyFront.jpg" alt="Bike Happy Front" >
+  $pictures = "<div class='row'>
+                <div class='col-md-3 block'>
+                  <img class='img-responsive' src='../$itemsArray[5]' alt='$itemsArray[6]'>
                 </div>
-                <div class="col-md-3 block">
-                  <img class="img-responsive" src="../statics/images/shop/chromeSpeedFront.jpg" alt="Chrome Speed Front" >
+                <div class='col-md-3 block'>
+                  <img class='img-responsive' src='../statics/images/shop/chromeSpeedFront.jpg' alt='Chrome Speed Front' >
                 </div>
-                <div class="col-md-3 block">
-                  <img class="img-responsive" src="../statics/images/shop/spinToWinFront.jpg" alt="Spin it to win it front" >
+                <div class='col-md-3 block'>
+                  <img class='img-responsive' src='../statics/images/shop/spinToWinFront.jpg' alt='Spin it to win it front' >
                 </div>
-                <div class="col-md-3 block">
-                  <img class="img-responsive" src="../statics/images/shop/utilityFront.jpg" alt="Utility Front" >
+                <div class='col-md-3 block'>
+                  <img class='img-responsive' src='../statics/images/shop/utilityFront.jpg' alt='Utility Front' >
                 </div>
-              </div>';
+              </div>";
   $content = $pictures;
   $script = '<script src="../statics/scripts.js"></script>';
   $header = "Shop";
   require "statics/layout.php";
   // print_r($itemsArray);
-  echo "<br>$itemsArray[0]<br>";
-  echo "<br>$itemsArray[1]<br>";
-  echo "<br>$itemsArray[2]<br>";
-  echo "<br>$itemsArray[3]<br>";
-  echo "<br>$itemsArray[4]<br>";
-  echo "<br><img class='img-responsive' src='../$itemsArray[5]' alt='$itemsArray[6]'><br>";
+  // echo "<br>$itemsArray[0]<br>";
+  // echo "<br>$itemsArray[1]<br>";
+  // echo "<br>$itemsArray[2]<br>";
+  // echo "<br>$itemsArray[3]<br>";
+  // echo "<br>$itemsArray[4]<br>";
+  // echo "<br><img class='img-responsive' src='../$itemsArray[5]' alt='$itemsArray[6]'><br>";
 
 ?>
