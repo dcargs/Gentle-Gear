923d8bc174fb6cbc09cc81aefde66c4f04e17f95
dcargs <cargilldevin@gmail.com>
2017-04-27 10:25:44 -0500

    asdf

--- shop/shop.php
+++ shop/shop.php
@@ -1,17 +1,17 @@
 <?php
 
   $pictures = '<div class="row">
-                <div class="col-md-3">
+                <div class="col-md-3 block">
                   <img src="../statics/images/shop/bikeHappyFront.jpg" alt="Bike Happy Front" >
                 </div>
-                <div class="col-md-3">
-                  <img src="../statics/images/shop/chromeSpeedFront.JPG" alt="Chrome Speed Front" >
+                <div class="col-md-3 block">
+                  <img src="../statics/images/shop/chromeSpeedFront.jpg" alt="Chrome Speed Front" >
                 </div>
-                <div class="col-md-3">
-                  <img src="../statics/images/shop/spinToWinFront.JPG" alt="Spin it to win it front" >
+                <div class="col-md-3 block">
+                  <img src="../statics/images/shop/spinToWinFront.jpg" alt="Spin it to win it front" >
                 </div>
-                <div class="col-md-3">
-                  <img src="../statics/images/shop/utilityFront.JPG" alt="Utility Front" >
+                <div class="col-md-3 block">
+                  <img src="../statics/images/shop/utilityFront.jpg" alt="Utility Front" >
                 </div>
               </div>';
   $content = $pictures;
