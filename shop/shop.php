5d80ab97fc4767f4468ae649b173ab7591111665
dcargs <cargilldevin@gmail.com>
2017-04-27 14:14:53 -0500

    asdf

--- shop/shop.php
+++ shop/shop.php
@@ -6,7 +6,7 @@
   $content .= $modal;
   $pictures .= "<div id='items'>";
   $pictures .= "<div class='row'>";
-  $modalSrc = "'../".$result[3]."'";
+  $modalSrc = "'".$result[3]."'";
   $pictures .= "<div class='picture col-md-3 block'><img onclick='modalFill($modalSrc)' class='img-responsive' src='../$result[3]' alt='$result[4]'></div>";
   $pictures .= "</div></div>";
   $content .= $pictures;
