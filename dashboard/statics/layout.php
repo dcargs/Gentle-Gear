0e4962b80c1258df9a8e1a592d6998bd5322d8a3
dcargs <cargilldevin@gmail.com>
2017-04-26 23:39:24 -0500

    asdf

--- dashboard/statics/layout.php
+++ dashboard/statics/layout.php
@@ -3,11 +3,11 @@
 <html>
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../statics/styles.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <?= $script ?>
+    <link rel="stylesheet" href="../../statics/styles.css">
     <meta charset="utf-8">
     <title>Gentle Gear</title>
   </head>
