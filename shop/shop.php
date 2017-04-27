a2baa5ee71e599b0cfa9d383f2d492960cbd255e
dcargs <cargilldevin@gmail.com>
2017-04-27 14:22:52 -0500

    wrong js file

--- shop/shop.php
+++ shop/shop.php
@@ -10,7 +10,7 @@
   $pictures .= "<div class='picture col-md-3 block'><img onclick='modalFill($modalSrc)' class='img-responsive' src='../$result[3]' alt='$result[4]'></div>";
   $pictures .= "</div></div>";
   $content .= $pictures;
-  $script = '<script src="../statics/scripts.js"></script>';
+  $script = '<script src="statics/shopScripts.js"></script>';
   $header = "Shop";
   require "statics/layout.php";
 ?>
