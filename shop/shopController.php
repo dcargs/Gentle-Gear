297f30f40372e3ac7c261830276d00cb10316807
dcargs <cargilldevin@gmail.com>
2017-04-27 14:34:49 -0500

    asdf

--- shop/shopController.php
+++ shop/shopController.php
@@ -25,7 +25,7 @@
                     <!-- Wrapper for slides -->
                     <div class="carousel-inner" role="listbox">
                       <div class="item active">
-                        <img class="pictureModal img-responsive" src="">
+                        <img class="height pictureModal img-responsive" src="">
                       </div>
                     </div>
                   </div><hr>
