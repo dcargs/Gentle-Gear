6694f61a22fe4f375139948bc853416491fd52a2
dcargs <cargilldevin@gmail.com>
2017-04-27 09:53:39 -0500

    no more login

--- statics/layout.php
+++ statics/layout.php
@@ -16,61 +16,23 @@
   <body>
     <div class="container">
       <div class="jumbotron">
-        <?php if($currentPage == 'login.php'){
-
-        } ?>
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-          <!-- Indicators -->
-          <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <!-- <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-            <li data-target="#myCarousel" data-slide-to="3"></li> -->
-          </ol>
-
-          <!-- Wrapper for slides -->
-          <div class="carousel-inner" role="listbox">
-
-            <div class="item active">
-              <img src="statics/images/car1.JPG" alt="Gentle Gear" >
-            </div>
-
-            <!-- <div class="item">
-              <img src="img_chania2.jpg" alt="Chania" width="460" height="345">
-              <div class="carousel-caption">
-                <h3>Chania</h3>
-                <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+        <?php if($currentPage != 'login.php'){
+          ?>
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            </ol>
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                <img src="statics/images/car1.JPG" alt="Gentle Gear" >
               </div>
             </div>
-
-            <div class="item">
-              <img src="img_flower.jpg" alt="Flower" width="460" height="345">
-              <div class="carousel-caption">
-                <h3>Flowers</h3>
-                <p>Beautiful flowers in Kolymbari, Crete.</p>
-              </div>
-            </div>
-
-            <div class="item">
-              <img src="img_flower2.jpg" alt="Flower" width="460" height="345">
-              <div class="carousel-caption">
-                <h3>Flowers</h3>
-                <p>Beautiful flowers in Kolymbari, Crete.</p>
-              </div>
-            </div> -->
-
           </div>
+      <?php  } ?>
 
-          <!-- Left and right controls -->
-          <!-- <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a> -->
-        </div>
+
         <div id="menu">
           <nav role="navigation" class="navbar navbar-default">
             <!-- Brand and toggle get grouped for better mobile display -->
