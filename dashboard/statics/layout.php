e5283caf5778bc36bab9b8ab55abd752447b3bb0
dcargs <cargilldevin@gmail.com>
2017-04-26 23:35:10 -0500

    button

--- dashboard/statics/layout.php
+++ dashboard/statics/layout.php
@@ -35,7 +35,7 @@
                     <li><a href="../../contact.php">Contact</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="btn-danger"><a href="../../logout.php">Logout</a></li>
+                    <li><a class="btn-danger" href="../../logout.php">Logout</a></li>
                 </ul>
             </div>
           </nav>
