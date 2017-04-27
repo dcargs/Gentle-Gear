a5287201a8178db254e615bafd1adfbc6664449a
dcargs <cargilldevin@gmail.com>
2017-04-26 23:53:11 -0500

    css

--- statics/layout.php
+++ statics/layout.php
@@ -8,6 +8,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <?= $script ?>
+    <link rel="stylesheet" href="statics/styles.css">
     <meta charset="utf-8">
     <title>Gentle Gear</title>
   </head>
