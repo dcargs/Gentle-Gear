b25cfad9462a9fd14f4eba51071eaf42852bef84
dcargs <cargilldevin@gmail.com>
2017-04-27 14:30:57 -0500

    modal change

--- shop/shopController.php
+++ shop/shopController.php
@@ -19,7 +19,7 @@
                 </div>
                 <div class="modal-body">
                   <div class="row">
-                    <div class="col-md-3 block">
+                    <div class="col-md-12 block">
                       <img class="pictureModal img-responsive" src="">
                     </div>
                   </div><hr>
