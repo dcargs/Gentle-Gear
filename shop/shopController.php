24e65bcf0fa1c84b09efd6342f12609ca4efe431
dcargs <cargilldevin@gmail.com>
2017-04-27 14:06:04 -0500

    duh

--- shop/shopController.php
+++ shop/shopController.php
@@ -7,7 +7,7 @@
   }
 
   function printModal(){
-    echo '<!-- Modal -->
+    return '<!-- Modal -->
           <div class="modal fade" id="myModal" role="dialog">
             <div class="modal-dialog">
 
