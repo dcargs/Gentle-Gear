7617741a51824403d1e285165a68dc0eb5db7950
dcargs <cargilldevin@gmail.com>
2017-04-27 19:52:08 -0500

    typo for days

--- dashboard/views/dashboard.php
+++ dashboard/views/dashboard.php
@@ -3,7 +3,7 @@
   statusAuthenticated();
 
   $content = adminView();
-  $content .+ printModal();
+  $content .= printModal();
   $script = '<script src="../statics/dashboardScripts.js"></script>';
   $header = "Dashboard";
   require "../statics/layout.php";
