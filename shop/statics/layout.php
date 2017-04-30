8443ecaa086b6435d98a4c77df955fa82e7e3f18
dcargs <cargilldevin@gmail.com>
2017-04-30 18:11:54 -0500

    asdf

--- shop/statics/layout.php
+++ shop/statics/layout.php
@@ -18,8 +18,8 @@
         <!-- Header Picture -->
         <div class="carousel-inner" role="listbox">
           <div class="item active">
-            <img src="../statics/images/car1.JPG" alt="Gentle Gear" >
-          </div>
+            <img  alt="Gentle Gear" >
+          </div><!-- src="../statics/images/car1.JPG" -->
         </div>
 
         <div id="menu">
