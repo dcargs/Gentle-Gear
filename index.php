69b4156d6cf8bc879c7554a6f5e9c90d07d0d272
dcargs <cargilldevin@gmail.com>
2017-04-30 21:33:07 -0500

    asdf

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
   session_start();
   $content = "<div class='row'>
-                <p>
-                Gentle Gear was founded in 2016 with the simple goal of putting more kids on bikes. Partnering with after school academic support programs, Gentle Gear uses the sales from our cycling clothing and merchandise to buy bicycles for young people who can't afford their own. Explore our unique designs on the shop page, read more about our mission and community activities on our about page
-                </p>
+                <div class='col-md-offset-1 col-md-10'>
+                  <p>
+                  Gentle Gear was founded in 2016 with the simple goal of putting more kids on bikes. Partnering with after school academic support programs, Gentle Gear uses the sales from our cycling clothing and merchandise to buy bicycles for young people who can't afford their own. Explore our unique designs on the shop page or read more about our mission and community activities on our about page
+                  </p>
+                </div>
               </div>";
   $message = "";
   $script = '<script src="statics/scripts.js"></script>';
