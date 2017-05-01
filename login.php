25493fe59c077ef9809f9ea14f53d280f18f2b18
dcargs <cargilldevin@gmail.com>
2017-04-30 19:31:10 -0500

    asdf

--- login.php
+++ login.php
@@ -1,16 +1,21 @@
 <?php
+  session_start();
   ob_start();
-  require "userController.php";
 
-  $message = "<br><div id='messageRow' class='row'>
-                <div class='alert alert-danger'>
-                  <strong>Error!</strong><span id='message'></span>
-                </div>
-              </div>";
-  $script = '<script src="statics/scripts.js"></script>
-             <script src="statics/userScripts.js"></script>';
-  $header = "Login";
-  $content = $loginForm;
-  $content .= $signupForm;
-  require "statics/layout.php";
+  if(isset($_SESSION['status'])){
+    header("Location: dashboard/views/dashboard.php");
+  } else {
+    require "userController.php";
+    $message = "<br><div id='messageRow' class='row'>
+                  <div class='alert alert-danger'>
+                    <strong>Error!</strong><span id='message'></span>
+                  </div>
+                </div>";
+    $script = '<script src="statics/scripts.js"></script>
+               <script src="statics/userScripts.js"></script>';
+    $header = "Login";
+    $content = $loginForm;
+    $content .= $signupForm;
+    require "statics/layout.php";
+  }
 ?>
