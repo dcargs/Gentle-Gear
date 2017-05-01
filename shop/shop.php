a22cfcd8a281e103e434cbe18eb1a1f77f8dbf4a
dcargs <cargilldevin@gmail.com>
2017-04-30 21:35:56 -0500

    asdf

--- shop/shop.php
+++ shop/shop.php
@@ -8,7 +8,7 @@
   $pictures = buildPictures();
   $content .= $pictures;
   $content = "<div class='row'>
-                *** click on a picture to view details about item ***
+                <h3 class='text-center'>*** click on a picture to view details about item ***</h3>
               </div>" . $content;
   $script = '<script src="statics/shopScripts.js"></script>';
   $header = "Shop";
