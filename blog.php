c4d7c3569946fda8540eec1949c825bd69311976
dcargs <cargilldevin@gmail.com>
2017-04-30 21:45:11 -0500

    asdf

--- blog.php
+++ blog.php
@@ -2,11 +2,13 @@
   session_start();
 
   $message = "";
-  $content = "<div class='container'>
-                <div class='row'>
-                  
+  $post = "<br><div class='container'>
+                <div class='panel panel-primary'>
+                  <div class='panel-heading'>Post Title</div>
+                  <div class='panel-body'>Post Content</div>
                 </div>
               </div>";
+  $content = $post . $post . $post;
   $script = '<script src="statics/scripts.js"></script>';
   $header = "Blog";
   require "statics/layout.php";
