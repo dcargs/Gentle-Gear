d586aaa7ed4bc3d7d56705261315036b305147af
dcargs <cargilldevin@gmail.com>
2017-04-30 22:16:53 -0500

    asfd

--- contact.php
+++ contact.php
@@ -1,6 +1,23 @@
 <?php
   session_start();
-  $content = "";
+  $content = "<br><div class='container'>
+                <div class='form-group'>
+                  <label>First Name:</label>
+                  <input type='text' class='form-control' placeholder='First Name'>
+                </div>
+                <div class='form-group'>
+                  <label>Last Name:</label>
+                  <input type='text' class='form-control' placeholder='Last Name'>
+                </div>
+                <div class='form-group'>
+                  <label>Phone Number:</label>
+                  <input type='number' class='form-control' placeholder='ex.6364976542'>
+                </div>
+                <div class='form-group'>
+                  <label>First Name:</label>
+                  <textarea class='form-control' placeholder='Message'></textarea>
+                </div>
+              </div>";
   $message = "";
   $script = '<script src="statics/scripts.js"></script>';
   $header = "Contact";
