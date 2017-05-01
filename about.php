bb6f1e8cecdb8bdaa5175f34487429963ae6576d
dcargs <cargilldevin@gmail.com>
2017-04-30 21:58:22 -0500

    asdf

--- about.php
+++ about.php
@@ -1,7 +1,49 @@
 <?php
   session_start();
   $message = "";
-  $content = "";
+  $content = "<div class='container'>
+                <br><div class='row'>
+                  <div class='col-md-offset-1 col-md-10'>
+                    <p>
+                      Gentle Gear was founded in 2016 with a simple purpose: to get more kids on bikes. Using a portion of our profits from the sale of cycling clothing and merchandise, Gentle Gear purchases new and gently used bikes for children in Columbia, Missouri who can't afford them
+                    </p>
+                    <p>
+                      We believe that riding bicycles is the key to a happy and healthy life, and that everyone, regardless of age or circumstance, should be able to enjoy the simple pleasure of riding a bike.
+                    </p>
+                  </div>
+                </div>
+                <div class='row'>
+                  <div class='col-md-offset-1 col-md-10'>
+                    <img src='statics/images/about/Jonah_Riding_large.png' class='img-responsive img-rounded' alt='Anita Riding'>
+                  </div>
+                </div>
+              </div>
+              <div class='row'>
+                <div class='col-md-offset-1 col-md-10'>
+                  <p>
+                    Staying safe on your bike is of paramount importance, which is why each child participates in a rider safety course and receives a helmet when they receive their bicycle.
+                  </p>
+                  <p>
+                    Bicycle safety and maintenance courses are open to the public and hosted on a monthly basis from March-October.
+                  </p>
+                </div>
+              </div>
+              <div class='row'>
+                <div class='col-md-offset-1 col-md-10'>
+                  <img src='statics/images/about/Anita_Riding_large.png' class='img-responsive img-rounded' alt='Anita Riding'>
+                </div>
+              </div>
+            </div>
+            <div class='row'>
+              <div class='col-md-offset-1 col-md-10'>
+                <p>
+                  We are currently partnered with after school academic support programs to distribute bicycles to underprivileged children. If you would like more information about Gentle Gear, or to see if your program qualifies to partner with Gentle Gear, please send us an e-mail through our contact page.
+                </p>
+                <p>
+                  Happy Riding!
+                </p>
+              </div>
+            </div>";
   $script = '<script src="statics/scripts.js"></script>';
   $header = "About";
   require "statics/layout.php";
