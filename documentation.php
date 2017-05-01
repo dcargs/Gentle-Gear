f4d071f41ad70007a57f1c139350fbf6c9882d61
dcargs <cargilldevin@gmail.com>
2017-04-30 22:42:05 -0500

    asdf

---
+++ documentation.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <title>Final Project Documentation</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="jumbotron">
+        <div class="row">
+          <div class="col-md-offset-1 col-md-10">
+            <h2 class='text-center'>Final Project Documentation</h2>
+          </div><hr>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>1.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>2.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>3.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>4.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>5.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>6.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>7.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>8.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>9.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>10.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>11.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>12.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>13.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>14.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>15.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>16.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>17.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>18.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-2">
+            <label>19.</label>
+          </div>
+          <div class="col-md-8">
+            explanation of how I did this
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </body>
+</html>
