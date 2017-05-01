fa831d0178dadf62542c52e1fb8a8e7d611c6287
dcargs <cargilldevin@gmail.com>
2017-04-30 21:42:30 -0500

    asdf

--- blog.php
+++ blog.php
@@ -2,7 +2,11 @@
   session_start();
 
   $message = "";
-  $content = "";
+  $content = "<div class='container'>
+                <div class='row'>
+                  
+                </div>
+              </div>";
   $script = '<script src="statics/scripts.js"></script>';
   $header = "Blog";
   require "statics/layout.php";
--- shop/shop.php
+++ shop/shop.php
@@ -8,7 +8,7 @@
   $pictures = buildPictures();
   $content .= $pictures;
   $content = "<div class='row'>
-                <h4 class='text-center'>*** Click on an item to view it's details ***</h4>
+                <h4 class='text-center'>*** Click on an item to view its details ***</h4>
               </div><br>" . $content;
   $script = '<script src="statics/shopScripts.js"></script>';
   $header = "Shop";
