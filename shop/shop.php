13cd087727515c007821c74eea8b423e60a61c81
dcargs <cargilldevin@gmail.com>
2017-04-30 21:35:11 -0500

    asdf

--- shop/shop.php
+++ shop/shop.php
@@ -7,6 +7,9 @@
   $content .= $modal;
   $pictures = buildPictures();
   $content .= $pictures;
+  $content = "<div class='row'>
+                *** click on a picture to view details about item ***
+              </div>" . $content;
   $script = '<script src="statics/shopScripts.js"></script>';
   $header = "Shop";
   require "statics/layout.php";
