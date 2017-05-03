5fd257c88c8a3e019ac3439bf0e09fe211ad1f95
dcargs <cargilldevin@gmail.com>
2017-05-02 22:48:57 -0500

    asdf

--- statics/scripts.js
+++ statics/scripts.js
@@ -15,9 +15,11 @@ function contactFormSubmission(){
         var content = data.posts[0]['content'];
         var boxFront = data.message_box[0]['content-front'];
         var boxMiddle = data.message_box[0]['content-middle'];
-        var boxEnd = data.message_box[0]['content-end'];
+        var boxEnd = data.message_box[0]['content-back'];
 
-        console.log(boxFront + title + boxMiddle + content + boxEnd);
+        var display = boxFront + title + boxMiddle + content + boxEnd;
+        $("#message").html(display)
+        console.log(display);
         // console.log(data.posts[0]['content']);
 
     });
