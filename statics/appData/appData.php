6b0c1a261152cce848117c25327330cb15e7d400
dcargs <cargilldevin@gmail.com>
2017-05-02 19:35:28 -0500

    wut do

--- statics/appData/appData.php
+++ statics/appData/appData.php
@@ -2,17 +2,9 @@
 
   if(isset($_GET['content'])){
     echo "set";
+    $appData = json_decode(file_get_contents('appData.json'));
+    print_r($appData);
   } else {
     echo "not set";
   }
-  // $id = empty($_GET['content']) ? 0 : $_GET['content'];
-  // $appData = json_decode(file_get_contents('appData.json'));
-  //
-  // header('Content-Type: application/json');
-  // if ($id == 0) {
-  //   print json_encode($appData);
-  // }
-  // else {
-  //   print json_encode($appData->posts[$id-1]);
-  // }
  ?>
