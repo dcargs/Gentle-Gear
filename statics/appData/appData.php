93d494a5791fa914246639e0bd94f66c02787962
dcargs <cargilldevin@gmail.com>
2017-05-02 19:56:36 -0500

    asdf

--- statics/appData/appData.php
+++ statics/appData/appData.php
@@ -2,7 +2,7 @@
 
   if(isset($_GET['content'])){
     $appData = json_decode(file_get_contents('appData.json'));
-    echo $appData->posts;
+    echo $appData->posts['content'];
   } else {
     echo "not set";
   }
